cffe431d6b87b701a221ce99b361583ed2060563
Anna Rychkova <134734366+AnnaRychkova1@users.noreply.github.com>
2024-08-05 13:18:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use("/avatars", express.static(path.resolve("public/avatars")));
-
 app.use("/api/users", userRouter);
 app.use("/api/water", authMiddleware, waterRouter);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
